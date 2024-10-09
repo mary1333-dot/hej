2bb14716ce1e7017ca44855de4bee0aa352ea2d1
mary1333-dot <mary1333@outlook.dk>
2024-10-09 10:03:02 +0200

    Opdater main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,6 +5,9 @@ input.onButtonPressed(Button.A, function () {
     basic.showIcon(IconNames.Heart)
     basic.showString("verdens bedste mor ")
 })
+input.onButtonPressed(Button.AB, function () {
+    basic.showIcon(IconNames.Heart)
+})
 input.onButtonPressed(Button.B, function () {
     basic.showString("min familie er den bedst ")
 })
