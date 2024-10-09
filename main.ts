f79b2b6779d2bca949c053362cc19a432d735811
mary1333-dot <mary1333@outlook.dk>
2024-10-09 10:01:47 +0200

    De første filer til MakeCode-projekt

---
+++ main.ts
@@ -0,0 +1,13 @@
+input.onPinReleased(TouchPin.P0, function () {
+	
+})
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Heart)
+    basic.showString("verdens bedste mor ")
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("min familie er den bedst ")
+})
+basic.forever(function () {
+	
+})
